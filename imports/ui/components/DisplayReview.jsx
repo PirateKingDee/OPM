1384f4343e7dcae0e284ceff0c1be3bdc86476b9
PirateKingDee <andyliang987@hotmail.com>
2017-06-27 10:38:56 -0700

    update review 2

--- imports/ui/components/DisplayReview.jsx
+++ imports/ui/components/DisplayReview.jsx
@@ -5,20 +5,28 @@ export default class DisplayReivew extends Component {
   render() {
     let reviews = this.props.reviews;
     let allReviews = null;
+    let getDate = (date) => {
+      return date.getMonth()+1+"/"+date.getDate()+", "+date.getFullYear();
+    }
+    // let dateString = review.date.getMonth()+1+"/"+review.date.getDate()+", "+review.date.getFullYear();
     //console.log('DisplayReivew', product);
     if(reviews!=null){
       allReviews = reviews.map(function(review, i){
 
-        return <div key={i}>
+      // console.log('dateString', dateString);
+        return <div key={i} className="display-review">
                 <div className="row">
-                  <Rating rating={review.rating} name={review.name} date={review.date}/>
+                  <Rating rating={review.rating} />
                   {review.verified? <span>Verified</span> : <span>Not verified</span>}
+                </div>
+                <div className="row">
+                  <span >By: </span><span className="username" >{review.name}</span>
+                  <span>On </span><span>{getDate(review.date)}</span>
                 </div>
                 <div key={i} className="row">
                     <span  key={i}>{review.review} </span>
                 </div>
               </div>;
-
       });
     }
     return (
